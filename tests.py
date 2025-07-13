464151de3ca275774df5679ed289d305eb4b9c1a
Riyad <alyakeenriyad2003@gmail.com>
2025-07-13 12:19:07 +0300

    Delete tests.py

--- tests.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Pizza	
-class PizzaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pizza
-        fields = '__all__'
-from django.test import TestCase
-
-# Create your tests here.
