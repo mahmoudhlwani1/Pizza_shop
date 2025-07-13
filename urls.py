2cf9d5266aee82fe9bdb9f4fbbbe5f4b6d3ce32c
Riyad <alyakeenriyad2003@gmail.com>
2025-07-13 12:18:55 +0300

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from orders.views import OrderViewSet                                                                
-from rest_framework.routers import DefaultRouter                                             
-router = DefaultRouter()                                                                                          
-router.register(r'orders', OrderViewSet)                                                                
-urlpatterns = [                                                                                                         
-    path('api/', include(router.urls)),                                                         
-    path('api-auth/', include('rest_framework.urls')),  ]                            
